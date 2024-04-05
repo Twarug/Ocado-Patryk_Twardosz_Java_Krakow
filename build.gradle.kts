c436ab24d64d95328abe17910b234beb298e210c
Twarug <patryk280502@gmail.com>
2024-04-05 22:45:54 +0200

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("java")
+}
+
+group = "com.ocado"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ src/main/java/com/ocado/basket/Main.java
@@ -0,0 +1,7 @@
+package com.ocado.basket;
+
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
