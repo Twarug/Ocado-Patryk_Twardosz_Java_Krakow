f65014381b43399744b2e8346342fa9bc57ed7f9
Twarug <patryk280502@gmail.com>
2024-04-05 22:54:37 +0200

    Add loading of config file

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -1,20 +1,35 @@
 package com.ocado.basket;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BasketSplitter {
-    
+
+    private final static Type DELIVEY_MAP_TYPE = new TypeToken<Map<String, List<String>>>() {}.getType();
+
+    private final Map<String, List<String>> productTypeToDeliveryTypesMap;
+
     /**
      * Constructor
      * 
      * @param absolutePathToConfigFile The absolute path to the configuration file
      */    
-    public BasketSplitter(String absolutePathToConfigFile) {
-        
+    public BasketSplitter(String absolutePathToConfigFile) throws FileNotFoundException {
+        Gson gson = new Gson();
+
+        // Read the configuration file
+        var fileReader = new JsonReader(new FileReader(absolutePathToConfigFile));
+        productTypeToDeliveryTypesMap = gson.fromJson(fileReader, DELIVEY_MAP_TYPE);
     }
-    
+
     /**
      * Splits the basket into multiple baskets based on the delivery method.
      * 
@@ -23,7 +38,7 @@ public class BasketSplitter {
      */
     public Map<String, List<String>> splitBasket(List<String> basket) {        
         Map<String, List<String>> splitBasket = new HashMap<>();
-        
+
         return splitBasket;
     }
 }
